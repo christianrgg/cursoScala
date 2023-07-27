16a29d3c350a975bcb73f4822ab7aefb6a045060
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-07-27 17:19:31 -0600

    Crear aplicaciones con intelliJ idea: Crear una aplicación con el metodo main

--- PR1/src/main/scala/curso/HolaMundo.scala
+++ PR1/src/main/scala/curso/HolaMundo.scala
@@ -1,12 +1,7 @@
-// 1. Los packages son directorios. Se crean con clic derecho -> new -> package
 package curso
 
-
-// 2. Crear objeto clic derecho -> new -> object
-/*
- Un objeto se usa para agrupar datos y comportamiento, contiene campos, métodos y otras definiciones de miembros. Tambien se usan para crear tipos
- */
-// 3. Agregar "extends" para heredar una intefaz (aqui se llama trait) y la palabra App lo convierte en una aplicación de Scala (proporciona un método main())
-object HolaMundo extends App {
-  println("Esto es un ejemplo App de escala")
+object HolaMundo {
+  def main( args: Array[String]): Unit = {
+  println("Esto es un ejemplo App de scala")
+  }
 }
